3ac5dfc42d339df6ab8ce99ee6997e6214ec9e9c
Rishi Kumar <rishijha238@gmail.com>
2024-02-04 08:15:03 +0530

    code commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TANX.FI-TASK',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    author='Rishi Kumar',
+    author_email='rishi.kumar2020@vitbhopal.ac.in',
+    description='Infrastructure Engineer Task given by tanX.fi',
+)
